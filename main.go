a84df2c0c2f1a74604a062059f8a8ecb45a6251d
k1 <263635591@qq.com>
2021-12-22 00:29:48 +0800

    feat: demo runnable

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-	_ "github.com/k1/go-blog/conf"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/k1/go-blog/conf"
 	_ "github.com/k1/go-blog/model"
 )
 
 func main() {
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "test",
+		})
+	})
 
+	port := conf.Conf.Server.Port
+	readTimeout := conf.ReadTimeout
+	writeTimeout := conf.WriteTimeout
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%s", port),
+		Handler:        router,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
