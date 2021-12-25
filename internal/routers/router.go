6beb51382531eb2cad37054116ff9e282997f5de
k1 <263635591@qq.com>
2021-12-25 22:14:46 +0800

    feat: add router to docs

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,12 +3,15 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/k1/go-blog/internal/models"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	tag := models.NewTag()
 	article := models.NewArticle()
